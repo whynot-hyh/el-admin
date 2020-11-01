740b5107d45986a75c499bd696c55fb545c9b94e
whynot-hyh <920780425@qq.com>
2020-11-01 23:28:31 +0800

    批量删除照片

--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -3,18 +3,28 @@
     <el-container style="position: absolute; top: 55px; left: 0; right: 0; bottom: 0;">
       <el-header class="d-flex align-items-center border-bottom">
         <div class="d-flex mr-auto">
-          <el-select class="mr-2" placeholder="请选择图片排序方式" v-model="searchForm.order" size="mini" style="width: 150px">
+          <el-select class="mr-2" placeholder="请选择图片排序方式"
+          v-model="searchForm.order" size="mini" style="width: 150px">
             <el-option label="区域一" value="shanghai"></el-option>
             <el-option label="区域二" value="beijing"></el-option>
           </el-select>
-          <el-input size="mini" class="mr-2" placeholder="输入相册名称"  v-model="searchForm.keyword" style="width: 150px"></el-input>
+          <el-input size="mini" class="mr-2" placeholder="输入相册名称"  
+          v-model="searchForm.keyword" style="width: 150px"></el-input>
           <el-button type="success" size="mini">搜索</el-button>
         </div>
-        <el-button type="success" size="mini" @click="openAlbumModel(false)">创建相册</el-button>
-        <el-button type="warning" size="mini" @click="uploadModel = true">上传图片</el-button>
+        <el-button type="warning" size="mini" 
+        @click="unChoose" v-if="chooseList.length">取消选中</el-button>
+        <el-button type="danger" size="mini" 
+        @click="imageDel({all:true})" v-if="chooseList.length">批量删除</el-button>
+        <el-button type="success" size="mini" 
+        @click="openAlbumModel(false)">创建相册</el-button>
+        <el-button type="warning" size="mini" 
+        @click="uploadModel = true">上传图片</el-button>
       </el-header>
       <el-container>
-        <el-aside width="200px" style="position: absolute; top: 60px; left: 0; bottom: 60px" class="bg-white border-right">
+        <el-aside width="200px" 
+        style="position: absolute; top: 60px; left: 0; bottom: 60px" 
+        class="bg-white border-right">
           <ul class="list-group list-group-flush">
             <album-item v-for="(item,index) in albums" :key="index"
             :item="item" :index="index" 
@@ -29,9 +39,11 @@
             <el-row :gutter="10">
               <el-col :span="24" :lg="4" :md="6" :sm="8"
               v-for="(item,index) in imageList" :key="index">
-                <el-card class="box-card mb-3 position-relative" :body-style="{'padding':'0'}" shadow="hover" style="cursor:pointer">
+                <el-card class="box-card mb-3 position-relative" 
+                :body-style="{'padding':'0'}" shadow="hover" style="cursor:pointer">
                   <div class="border" :class="{'border-danger':item.isChecked}">
-                    <span class="badge badge-danger" v-if="item.isChecked" style="position:absolute;right:0;top:0;">
+                    <span class="badge badge-danger" 
+                    v-if="item.isChecked" style="position:absolute;right:0;top:0;">
                       {{item.checkOrder}}
                     </span>
                     <img class="w-100" style="height: 100px" :src="item.url" @click="choose(item)">
@@ -40,9 +52,12 @@
                     </div>
                     <div class="p-2 text-center">
                       <el-button-group>
-                        <el-button size="mini" @click="previewImage(item)" class="p-2" icon="el-icon-view"></el-button>
-                        <el-button size="mini" @click="imageEdit(item,index)" class="p-2" icon="el-icon-edit"></el-button>
-                        <el-button size="mini" @click="imageDel(index)" class="p-2" icon="el-icon-delete"></el-button>
+                        <el-button size="mini" @click="previewImage(item)" 
+                        class="p-2" icon="el-icon-view"></el-button>
+                        <el-button size="mini" @click="imageEdit(item,index)" 
+                        class="p-2" icon="el-icon-edit"></el-button>
+                        <el-button size="mini" @click="imageDel({index})" 
+                        class="p-2" icon="el-icon-delete"></el-button>
                       </el-button-group>
                     </div>
                   </div>
@@ -53,7 +68,26 @@
           </el-main>
         </el-container>
       </el-container>
-      <el-footer>Footer</el-footer>
+      <el-footer class="border-top d-flex align-items-center px-0">
+        <div style="width:200px;flex-shrink:0;" 
+        class="h-100 d-flex align-items-center justify-content-center border-right">
+          <el-button-group>
+            <el-button size="mini">上一页</el-button>
+            <el-button size="mini">下一页</el-button>
+          </el-button-group>
+        </div>
+        <div style="flex:1;" class="px-2">
+          <el-pagination
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+            :current-page="currentPage4"
+            :page-sizes="[100, 200, 300, 400]"
+            :page-size="100"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="400">
+          </el-pagination>
+        </div>
+      </el-footer>
     </el-container>
 
     <!-- 修改或创建相册 -->
@@ -140,7 +174,7 @@ export default {
         this.imageList.push({
           id: i,      
           url: 'https://tangzhe123-com.oss-cn-shenzhen.aliyuncs.com/Appstatic/qsbk/demo/datapic/40.jpg',
-          name: '图片',
+          name: '图片'+i,
           isChecked: false,
           checkOrder: 0
         })
@@ -227,18 +261,45 @@ export default {
       })
     },
     //删除图片
-    imageDel(index){
-      this.$confirm('是否删除该图片?', '提示', {
+    imageDel(obj){
+      this.$confirm(obj.all?'是否删除选中图片?':'是否删除该图片?', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
-      }).then(({ value }) => {
-        this.imageList.splice(index,1)
+      }).then(() => {
+        if(obj.all){
+          //批量删除
+          let list = this.imageList.filter(img => {
+            return !this.chooseList.some(c => c.id === img.id)
+          })
+          this.imageList = list
+          this.chooseList = []
+        }else{
+          this.imageList.splice(obj.index,1)
+        }
+      
         this.$message({
           type: 'success',
           message: '删除成功'
         });
       });  
+    },
+
+    //取消选中
+    unChoose(){
+      this.imageList.forEach(img => {
+        let i = this.chooseList.findIndex(item => {
+          return item.id === img.id        
+        })
+        if(i > -1){
+          //取消选中样色,选中排序归零
+          img.isChecked = false
+          img.checkOrder = 0
+          //从chooseList中移除
+          this.chooseList.splice(i,1)
+
+        }
+      })
     },
 
     //选中图片
@@ -268,7 +329,16 @@ export default {
 
       item.isChecked = false
       item.checkOrder = 0
+    },
+
+    //分页
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
     }
+
   }
 }
 </script>
